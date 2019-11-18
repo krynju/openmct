352783d8a5fc50640aeab443f597c37537ed0e56
krynju <gulinski.krystian@gmail.com>
2019-11-18 20:32:46 +0100

    collectionProvider added

--- src/plugins/testPlugin/plugin.js
+++ src/plugins/testPlugin/plugin.js
@@ -7,11 +7,42 @@ async function fetchData() {
     return await response.json();
 }
 
-define([
+async function getObject(key) {
+    let response = await fetch(BACKEND_ADDRESS + '/objects' + '?key=' + key.toString(), {method: "get"});
+    return await response.json()
+}
 
-], function (
+async function getComposition(key) {
+    let response = await fetch(BACKEND_ADDRESS + '/composition' + '?key=' + key.toString(), {method: "get"});
+    return await response.json()
+}
 
-) {
+let objectProvider = {
+    get: function (identifier) {
+        return getObject(identifier.key).then(function (result) {
+            return {
+                identifier: identifier,
+                name: result.name,
+                type: result.type
+            };
+        });
+    }
+};
+
+
+var compositionProvider = {
+    appliesTo: function (domainObject) {
+        return domainObject.identifier.namespace === 'example.namespace';
+    },
+    load: function (domainObject) {
+        return getComposition(domainObject.identifier.key)
+            .then(function (result) {
+                return result;
+            })
+    }
+};
+
+define([], function () {
 
     let installed = false;
 
@@ -23,12 +54,19 @@ define([
             }
             installed = true;
 
-            console.log("hey test plugin is here");
-
             // openmct.legacyRegistry.enable("openmct/plot");
 
-            fetchData()
-                .then(data => console.log(data))
+
+            openmct.objects.addRoot({
+                namespace: 'example.namespace',
+                key: '5dd297fb6824251bd4883780'
+            });
+
+            openmct.objects.addProvider('example.namespace', objectProvider);
+
+            openmct.composition.addProvider(compositionProvider)
+
+
         };
     }
 
